cc940be70fda9f5010df5841e148b10e01d73e6a
Manoj Malviya <malviyamanoj1896@gmail.com>
2025-08-07 14:16:42 +0200

    init properly

---
+++ src/black_hole.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Manoj Malviya on 8/7/25.
+//
+
+#include "black_hole.h"
---
+++ src/black_hole.h
@@ -0,0 +1,8 @@
+//
+// Created by Manoj Malviya on 8/7/25.
+//
+
+#ifndef BLACK_HOLE_H
+#define BLACK_HOLE_H
+
+#endif //BLACK_HOLE_H
