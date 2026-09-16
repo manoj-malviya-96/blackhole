9dd495d6a68f48c6690fed4a87b3c6aa112872af
manoj <malviyamanoj1896@gmail.com>
2026-09-16 10:58:43 +0200

    move default camera further from the black hole

--- src/camera.h
+++ src/camera.h
@@ -56,13 +56,13 @@ public:
     }
 
     void reset() {
-        radius = 6.34e10f;
+        radius = 2.2e11f;
         azimuth = 0.0f;
         elevation = 1.0f;
     }
 
     QVector3D target{0.f, 0.f, 0.f};
-    float radius = 6.34e10f;
+    float radius = 2.2e11f;
     float minRadius = 1e10f;
     float maxRadius = 1e12f;
     float azimuth = 0.0f;
